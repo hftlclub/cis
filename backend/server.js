1d7327c43d8fb80a5c18c6d4b5d5f22930d742a3
Ferdinand Malcher (herbert) <mail@ferdinand-malcher.de>
2015-03-04 16:09:47 +0100

    API on /api subpath

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ var jwt = require('jwt-simple');
 
 
 var app = express();
+var api = express.Router();
 
 var ldap = require('./ldap');
 var config = require('./config');
@@ -14,21 +15,26 @@ var auth = require('./controllers/auth');
 var jwtauth = require('./middleware/jwtauth')
 var requireAuth = require('./middleware/requireauth');
 
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({
+api.use(bodyParser.json());
+api.use(bodyParser.urlencoded({
     extended: true
 }));
 
 
 
 
-app.post('/login', auth.login);
+api.post('/login', auth.login);
 
 
-app.get('/secret', jwtauth, requireAuth, function(req, res){
+api.get('/secret', jwtauth, requireAuth, function(req, res){
     res.send('Club Admin Backend ' + JSON.stringify(req.user));
 })
 
+
+//assign api router to /api
+app.use('/api', api);
+
+//start server
 app.listen(config.port, function(err){
     console.log('Club Admin Backend Server started on port ' + config.port);
 });
