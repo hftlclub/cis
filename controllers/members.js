bec96a52205382e68713a6f50a754eed907b6c15
Ferdinand Malcher (herbert) <mail@ferdinand-malcher.de>
2015-03-29 16:14:45 +0200

    first attempt for excel export #29

--- controllers/members.js
+++ controllers/members.js
@@ -1,10 +1,7 @@
-var util = require('util');
+var xlsx = require('node-xlsx');
 var moment = require('moment');
 var config = require('../config');
-var ldap = require('../modules/ldap');
-var smtp = require('../modules/smtp');
 var membersservice = require('../services/membersservice');
-var crypto = require('crypto');
 
 
 
@@ -13,6 +10,40 @@ exports.listmembers = function(req, res, next){
 	membersservice.getMembers(function(err, members){
 		if(err) return next(err);
 		res.json(members).end();
+	});
+}
+
+
+
+
+exports.makexlsx = function(req, res, next){
+	membersservice.getMembers(function(err, members){
+		if(err) return next(err);
+		
+		//column headers
+		var data = [['Nachname', 'Vorname', 'E-Mail', 'Spitzname', 'Telefon', 'Status', 'Position', 'Vereinsbeitritt', 'Ehrenmitglied', 'Teamdrive']];
+		
+		for(var i = 0; i < members.length; i++){
+			var status = (members[i].former) ? 'ehemalig' : 'aktiv';
+			var honorary = (members[i].honorary) ? 'x' : '';
+			
+			data.push([
+				members[i].lastname,
+				members[i].firstname,
+				members[i].email,
+				(members[i].alias) ? members[i].alias : '',
+				(members[i].tel) ? members[i].tel : '',
+				status,
+				members[i].role,
+				(members[i].accessiondate) ? moment(members[i].accessiondate).format('DD.MM.YYYY') : '',
+				honorary,
+				(members[i].teamdrive) ? members[i].teamdrive : '',
+			]);
+		}
+		
+		
+		var buffer = xlsx.build([{name: 'Mitgliederliste', data: data}]);
+		res.send(buffer);
 	});
 }
 
