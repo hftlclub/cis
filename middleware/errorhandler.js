8b788619cd9622e3c0b1ec28ff798f364acccb6d
Danny Koppenhagen <Danny.Koppenhagen@t-online.de>
2015-04-11 13:21:44 +0200

    show Date and time on error log

--- middleware/errorhandler.js
+++ middleware/errorhandler.js
@@ -1,13 +1,13 @@
 exports.validation = function(req, res, next){
 	var valerrors = req.validationErrors(true);
 	if(valerrors){
-		console.log(valerrors);
+		console.log(new Date()+':', valerrors);
 		res.status(400).json({validationerror: valerrors}).end();
 	}
 }
 
 
 exports.generic = function(err, req, res, next) {
-    console.error(err);
+    console.error(new Date()+':', err);
     res.status(err.status || 500).json({error: err.message});
 }
