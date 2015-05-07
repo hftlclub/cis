05a9b992024c9493cfac2746cf17638177fd9c08
Ferdinand Malcher (herbert) <mail@ferdinand-malcher.de>
2015-05-07 15:52:43 +0200

    map centering and labels overlay

--- frontend/js/floorplan-controller.js
+++ frontend/js/floorplan-controller.js
@@ -1,29 +1,50 @@
 clubAdminApp.controller('floorplanController', ['$scope', '$rootScope', '$http', '$routeParams', 'clubAuth', 'leafletData', 'leafletBoundsHelpers', function($scope, $rootScope, $http, $routeParams, clubAuth, leafletData, leafletBoundsHelpers) {
 
+	var bounds = [[0, 571],[920, 0]];
+	
+
   var maxBounds = leafletBoundsHelpers.createBoundsFromArray([
     [0, 571],
     [920, 0]
   ]);
 
   angular.extend($scope, {
-		maxBounds: maxBounds,
+	maxBounds: leafletBoundsHelpers.createBoundsFromArray(bounds),
     defaults: {
       crs: 'Simple',
       maxZoom: 4,
       minzoom: 1
 		},
+	center: {
+            lat: 460,
+            lng: 285.5,
+            zoom: 1
+        },
     layers: {
       baselayers: {
         floormap: {
           name: 'Club',
           type: 'imageOverlay',
           url: '../img/floorplan/floormap_base.svg',
-          bounds: maxBounds,
+          bounds: bounds,
+          layerParams: {
+            noWrap: true
+          }
+        }
+      },
+      overlays: {
+        labels: {
+          name: 'Labels',
+          visible: true,
+          type: 'imageOverlay',
+          url: '../img/floorplan/floormap_labels.svg',
+          bounds: bounds,
           layerParams: {
             noWrap: true
           }
         }
       }
+          
     }
   });
 
