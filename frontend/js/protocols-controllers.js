abc4a311ba3fcab7209f993ed3d2e3a95e83aeef
Danny Koppenhagen <Danny.Koppenhagen@t-online.de>
2015-07-24 21:10:13 +0200

    autosave on leave

--- frontend/js/protocols-controllers.js
+++ frontend/js/protocols-controllers.js
@@ -1,31 +1,24 @@
 // controller for protocol form
-clubAdminApp.controller('protocolFormController', function($scope, $http, $routeParams, $interval, $route, clubAuth) {
-  
-  
+clubAdminApp.controller('protocolFormController', function($scope, $http, $routeParams, $interval, $route, $window, clubAuth) {
+
+
   $scope.users = [];
 
- 
+
   var form = $scope.form = {
-  	id: $routeParams.id,
-  	mode: $route.current.locals.clubMode,
-  	errors: {},
-  	message: null,
-  	data: {
-	  attendants: [],
-	  start: null,
-	  end: null
-  	}
+    id: $routeParams.id,
+    mode: $route.current.locals.clubMode,
+    errors: {},
+    message: null,
+    data: {
+      attendants: [],
+      start: null,
+      end: null
+    }
   }
 
   $scope.commonTitles = ['Clubsitzung', 'Mitgliederversammlung', 'Planungstreffen'];
-  
-  
-  
-  $scope.testfunc = function(){
-	  console.log("dsd " + $scope.protocolForm.$dirty);
-  }
-  
-  
+
 
   // options for textbox
   $scope.aceOptions = {
@@ -48,77 +41,105 @@ clubAdminApp.controller('protocolFormController', function($scope, $http, $route
         this.stopTimer();
       }
     },
-    stopTimer: function(){
-	    $interval.cancel(this.interval)
+    stopTimer: function() {
+      $interval.cancel(this.interval)
     }
-};
-$scope.autoSave.setTimer(); //initially start timer
-	
-	//destroy timer on location change
-	$scope.$on("$destroy", function(){
-		$scope.autoSave.stopTimer();
-	});
+  };
+  $scope.autoSave.setTimer(); //initially start timer
+
+  //destroy timer on location change
+  $scope.$on("$destroy", function() {
+    $scope.autoSave.stopTimer();
+  });
+
+  // catch location change event and save if in edit mode
+  $scope.$on('$locationChangeStart', function(event, next, current) {
+    if (form.mode == 'edit') saveOnLeave(event);
+  });
+  // catch close/reload event and save if in edit mode
+  $window.addEventListener("beforeunload", function(event) {
+    if (form.mode == 'edit') saveOnLeave(event);
+  });
+
+  function saveOnLeave(event) {
+    // autosave if form is dirty
+    if ($scope.protocolForm.$dirty) {
+      $scope.save();
+    }
+
+    // open confirm dialog if location was changed
+    if (event.name == '$locationChangeStart') {
+      // TODO : Snackbar Feedback
+    }
+    // if  beforeunload event was fired (close tab, reload page)
+    else {
+      var confirmationMessage = '';
+      (event || window.event).returnValue = confirmationMessage; //Gecko + IE
+      return confirmationMessage; //Webkit, Safari, Chrome
+    }
+  }
+
 
 
   //models for timepickers // create new when form mode is "add"
   $scope.times = {
-	  date: null,
-	  start: null,
-	  end: null
+    date: null,
+    start: null,
+    end: null
   };
 
   //stuff for the "attendee is later" popover
   $scope.laterPopover = {
-	  template: '/templates/protocols/laterPopover.html' ,
-	  setInitial: function(att){
-		  att.later = new Date();
-	  }
+    template: '/templates/protocols/laterPopover.html',
+    setInitial: function(att) {
+      att.later = new Date();
+    }
   }
 
-  
+
   /* Date picker */
   //only create new datepicker if there's no data expected
   $scope.minDate = $scope.minDate ? null : new Date(2012, (10 - 1), 25);
   $scope.maxDate = $scope.maxDate ? null : new Date();
 
   $scope.datePicker = {
-	  format: 'dd.MM.yyyy',
-	  open: function($event) {
-	  	$event.preventDefault();
-	  	$event.stopPropagation();
-	  	this.opened = true;
-  	  },
-  	  opened: false,
-  	  options : {
-  	  	formatYear: 'yy',
-  	  	startingDay: 1
-  	  }
+    format: 'dd.MM.yyyy',
+    open: function($event) {
+      $event.preventDefault();
+      $event.stopPropagation();
+      this.opened = true;
+    },
+    opened: false,
+    options: {
+      formatYear: 'yy',
+      startingDay: 1
+    }
   }
-  
+
 
   //stuff for attendants
   $scope.attendants = {
-	  input: null,
-	  add: function(name) {
-	  	if (name) {
-	  	var match = false;
-	  	var attendee = {
-        	'name': name,
-			'type': 'member'
-      	}
+    input: null,
+    add: function(name) {
+      if (name) {
+        var match = false;
+        var attendee = {
+          'name': name,
+          'type': 'member'
+        }
 
-	  	// check if person is already attendee
-	  	for (var i = 0; i < form.data.attendants.length; i++) {
-        	if (form.data.attendants[i].name == attendee.name) match = true;
-      	}
+        // check if person is already attendee
+        for (var i = 0; i < form.data.attendants.length; i++) {
+          if (form.data.attendants[i].name == attendee.name) match = true;
+        }
 
-	  	if (!match) form.data.attendants.push(attendee);
+        if (!match) form.data.attendants.push(attendee);
       }
-  	},
-  	remove: function(index) {
-    	form.data.attendants.splice(index, 1)
-  	}
-  	
+    },
+    remove: function(index) {
+      form.data.attendants.splice(index, 1)
+    }
+
   }
 
 
@@ -126,13 +147,13 @@ $scope.autoSave.setTimer(); //initially start timer
   // check whether title is a common title, then load template if available
   $scope.checkTitle = function() {
     for (i = 0; i < $scope.commonTitles.length; i++) { //go through common titles
-      if($scope.commonTitles[i] == form.data.title && !form.data.text) { //if current title matches a common title and there's no text in the field
+      if ($scope.commonTitles[i] == form.data.title && !form.data.text) { //if current title matches a common title and there's no text in the field
         //get protocol and fill textfield with it
         $http.get('/templates/protocols/presets/clubsitzung.md').success(function(data) {
           form.data.text = data;
         });
       }
-   }
+    }
   }
 
 
@@ -140,48 +161,48 @@ $scope.autoSave.setTimer(); //initially start timer
   $scope.save = function() {
     //make ISOStrings from dates
     form.data.date = $scope.times.date.toISOString();
-	form.data.start = $scope.times.start.toISOString();
-	form.data.end = $scope.times.end.toISOString();
-	
+    form.data.start = $scope.times.start.toISOString();
+    form.data.end = $scope.times.end.toISOString();
 
-    if(form.mode == 'edit' && form.id){
+
+    if (form.mode == 'edit' && form.id) {
       $http.put(apiPath + '/protocols/' + form.id, form.data)
         .success(function(data) {
           form.message = 'successEdit';
         })
-        .error(function (data, status) {
-    			if (status == 400 && data.validationerror) {
-    				form.message = 'invalid';
-    				form.errors = data.validationerror;
-    			} else {
-    				form.data.errormessage = data;
-    				form.message = 'error';
-    				form.errors = null;
-    			}
+        .error(function(data, status) {
+          if (status == 400 && data.validationerror) {
+            form.message = 'invalid';
+            form.errors = data.validationerror;
+          } else {
+            form.data.errormessage = data;
+            form.message = 'error';
+            form.errors = null;
+          }
         });
-    
-    
-    } else if(form.mode == 'add') {
+
+
+    } else if (form.mode == 'add') {
       $http.post(apiPath + '/protocols', form.data)
         .success(function(data) {
           form.message = 'successAdd';
 
-		  //if ID is returned, switch to edit mode
-          if(data.id){
-	          form.mode = 'edit';
-			  form.id = data.id;
-		  }
-          
+          //if ID is returned, switch to edit mode
+          if (data.id) {
+            form.mode = 'edit';
+            form.id = data.id;
+          }
+
         })
-        .error(function (data, status) {
-    			if (status == 400 && data.validationerror) {
-    				form.message = 'invalid';
-    				form.errors = data.validationerror;
-    			} else {
-    				form.data.errormessage = data;
-    				form.message = 'error';
-    				form.errors = null;
-    			}
+        .error(function(data, status) {
+          if (status == 400 && data.validationerror) {
+            form.message = 'invalid';
+            form.errors = data.validationerror;
+          } else {
+            form.data.errormessage = data;
+            form.message = 'error';
+            form.errors = null;
+          }
         });
     }
   }
@@ -194,47 +215,47 @@ $scope.autoSave.setTimer(); //initially start timer
       data.forEach(function(row) {
         if (!row.former) {
           $scope.users.push(row.firstname + ' ' + row.lastname);
-		}
-	  });
-	});
-	  
+        }
+      });
+    });
 
-	//in edit mode: go and get the protocol we want to edit
-	if(form.mode == 'edit' && form.id){
-		// get data from specific protocol if mode is 'edit'
-		$http.get(apiPath + '/protocols/raw/' + form.id).success(function(data) {
-			form.data = data;
-			$scope.times.date = new Date(data.date);
-			$scope.times.start = new Date(data.start);
-			$scope.times.end = new Date(data.end);
-	    });
-	      
-	}
+
+    //in edit mode: go and get the protocol we want to edit
+    if (form.mode == 'edit' && form.id) {
+      // get data from specific protocol if mode is 'edit'
+      $http.get(apiPath + '/protocols/raw/' + form.id).success(function(data) {
+        form.data = data;
+        $scope.times.date = new Date(data.date);
+        $scope.times.start = new Date(data.start);
+        $scope.times.end = new Date(data.end);
+      });
+
+    }
 
   }
 
-  
-  
-  
+
+
+
   /******************************/
 
 
 
 
   refresh();
-  
+
   //default values when creating a new protocol
-  if(form.mode == 'add'){
-	  $scope.times.date = new Date();
-	  $scope.times.start = new Date();
-	  $scope.times.end = new Date();
-	  var me = clubAuth.user.firstname + ' ' + clubAuth.user.lastname;
-	  form.data.recorder = me;
-	  $scope.attendants.add(me);
+  if (form.mode == 'add') {
+    $scope.times.date = new Date();
+    $scope.times.start = new Date();
+    $scope.times.end = new Date();
+    var me = clubAuth.user.firstname + ' ' + clubAuth.user.lastname;
+    form.data.recorder = me;
+    $scope.attendants.add(me);
   }
-  
-  
-  
+
+
+
 
 });
 
