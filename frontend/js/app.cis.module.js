a5a7d3de59d5c0c66ab00a9296ecfce70283d8e4
TilmannBach <tilli14@msn.com>
2015-10-18 17:50:30 +0200

    + added full datatables support to keylist

--- frontend/js/app.cis.module.js
+++ frontend/js/app.cis.module.js
@@ -5,5 +5,13 @@ angular.module('app.cis', ['ngRoute', 'ui.bootstrap', 'ui.bootstrap.dropdown',
 
 angular.module('app.cis').run(function($http) {
     var token = (localStorage.getItem('accessToken')) ? localStorage.getItem('accessToken') : null;
+    // angular ajax requests:
     $http.defaults.headers.common['X-Access-Token'] = token;
+    // jQuery ajax request are used by dataTables: 
+    $.ajaxSetup({
+    beforeSend: function (xhr)
+    {
+       xhr.setRequestHeader('X-Access-Token',token);   
+    }
+});
 });
--- frontend/js/keylist-controller.js
+++ frontend/js/keylist-controller.js
@@ -1,56 +1,60 @@
-angular.module('app.cis').controller('KeyListController', function ($scope, $rootScope, $http, $routeParams, clubAuth, appConf, DTOptionsBuilder, DTColumnBuilder) {
+angular.module('app.cis').controller('KeyListController', function ($scope, $rootScope, $http, $routeParams, $compile, clubAuth, appConf, DTOptionsBuilder, DTColumnBuilder) {
     $scope.listFilter = '';
-
-    $scope.members = {};
-    $scope.members.data = null;
-
-    $scope.dt = {};
-    $scope.dt.options = DTOptionsBuilder.newOptions()
-        .withDisplayLength(9999)    // show all users
-        .withOption('ordering', false)
-        .withDOM('t')               // manipulate the default DT dom // t: show only table, no extra controls
-        .withLanguage({
-            "sZeroRecords": "Kein Eintrag entspricht dem eingetragenen Filter!"
-        });
-    $scope.dt.columDefs = {
-        "columnDefs": [
-            {
-                "render": function (data, rendertype, full, meta) {
-                    return full;
-                }
-            },
-            {},
-            {
-                render: function (data, rendertype, full, meta) {
-                    if (rendertype === 'display') {
-                        if (data) {
-                            return "<span class='glyphicon glyphicon-ok-sign'></span>";
-                        } else {
-                            return '';
-                        }
-                    }
-                    else {
-                        return data;
-                    }
-                }
-            },
-        ]
-    };
-    $scope.tableFiltered = function (filterText) {
-        $scope.dt.options.withOption('search', { "search": filterText });
-    }
 
     $scope.isSuperuser = $rootScope.clubUser.superuser;
 
     $scope.keys = appConf.doorKeyList;
 
-    refresh();
+    $scope.dt = {};
+    $scope.dt.instance = {};
+    $scope.dt.options = DTOptionsBuilder
+        .fromSource(appConf.api + '/keylist')
+        .withDisplayLength(9999)    // show all users
+        .withDOM('ti')              // manipulate the default DT dom // t: show only table, no extra controls
+        .withLanguage({
+            "sZeroRecords": "Kein Eintrag entspricht dem eingetragenen Filter!",
+            "sInfo": "Einträge: _TOTAL_",
+            "sInfoEmpty": "Es existieren keine Einträge",
+            "sInfoFiltered": "(gefiltert aus _MAX_ Einträgen)"
 
-    function refresh() {
-        $http.get(appConf.api + '/keylist').
-            success(function (data) {
-                $scope.members.data = data;
-            });
+        })
+        .withOption('initComplete', function (settings) {
+            // Recompiling so we can bind Angular directive to the DT; used for the tooltips in the table header
+            $compile(angular.element('#' + settings.sTableId).contents())($scope);
+        })
+    ;
+    // initialize the DT columns
+    $scope.dt.columns = [
+        DTColumnBuilder.newColumn('name').withTitle('Name').renderWith(function (data, rendertype, full) { return full.lastname + ', ' + full.firstname; })
+    ];
+    // initialize dynamic cols for all keys
+    angular.forEach($scope.keys, function (key) {
+        $scope.dt.columns.push(DTColumnBuilder.newColumn(null).withTitle('<div tooltip-placement="top" tooltip-trigger="mouseenter" tooltip="' + key.name + '">' + key.key + '</div>').withClass('text-center').renderWith(function (data, rendertype, full) {
+            if (full.keyPermissions[key.key]) {
+                if (rendertype === 'display') {
+                    return "<span class='glyphicon glyphicon-ok-sign'></span>";
+                } else {
+                    return key.key + ' ' + key.name;
+                }
+            }
+            return null;
+        }));
+    });
+    // initialize edit col if applicable
+    if ($scope.isSuperuser) {
+        $scope.dt.columns.push(DTColumnBuilder.newColumn('username').notSortable().withTitle('<span class="glyphicon glyphicon-cog"></span>').withClass('text-center hidden-print').renderWith(function (data, rendertype, full) {
+            if (rendertype === 'display') {
+                return '<a ng-href="#/users/edit/' + data + '" type="button" class="btn btn-xs btn-default"><span class="glyphicon glyphicon-pencil" > </span></a>';
+            }
+            else {
+                return data;
+            }
+        }));
+    }
+    //
+    
+    $scope.tableFiltered = function (filterText) {
+        $scope.dt.instance.DataTable.search(filterText).draw();
     }
 
     $scope.date = new Date();
@@ -61,6 +65,8 @@ angular.module('app.cis').controller('KeyListController', function ($scope, $roo
 
 
 angular.module('app.cis').controller('PublicKeyListController', function ($scope, $http, $routeParams, appConf) {
+    
+    // todo: what is this controller used for?
 
     $scope.members = {};
     $scope.members.data = null;
